ac8b0109349b189daf84eb0e10cec655f4063d6d
Sufang Wang <62132617+sufangwang-npu@users.noreply.github.com>
2022-03-24 19:01:51 +0800

    Delete ATAC-DEA_DataPretreatment directory

--- ATAC-DEA_DataPretreatment/www/message-handler.js
+++
@@ -1,8 +0,0 @@
-// This recieves messages of type "testmessage" from the server.
-// See http://shiny.rstudio.com/gallery/server-to-client-custom-messages.html
-// for details
-Shiny.addCustomMessageHandler("Message",
-  function(message) {
-    alert(JSON.stringify(message));
-  }
-);
